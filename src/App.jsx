3b2659c03030ce2c2ff2669def7d98762373586b
Max Sidhartha <max_rocha88@hotmail.com>
2024-09-24 10:57:13 -0300

    Primeiro commit react

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter,Route,Routes,Navigate } from "react-router-dom"
+import Cadastro from './pages/Cadastro'
+import Login from './pages/login'
+
+function App() {
+
+  return (
+    
+     
+      <BrowserRouter>
+      <Routes>
+       
+       <Route path = "/" element={<Cadastro />} />
+       <Route path = "/login" element={<Login />} />
+       
+      </Routes>
+      </BrowserRouter>
+      
+    
+  )
+
+
+
+}
+
+export default App
